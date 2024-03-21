be75d335b13f412f74ecfb6027fadf4670c8f22a
tansimakamalfiana <tansima.fiana@edu.sait.ca>
2024-03-20 18:50:30 -0600

    addChanges

---
+++ tansimakamalfiana-portfolio/app/pages/PersonalPortfolioPage/page.js
@@ -0,0 +1,151 @@
+"use client"
+
+import Image from "next/image";
+import GitHubIcon from "@/app/components/GitHubIcon"
+import BackButton from "@/app/components/BackToTopButton"
+
+import Navigation from "@/app/navigation/page"
+import { useState, useEffect } from "react"
+
+export default function PersonalPortfolioPage() {
+  const scrollY = useScroll();
+  const size = useWindowSize();
+
+  const activeTabClass = "py-4 px-8 bg-pink-50 rounded-t-lg "
+  const inactiveTabClass = "py-4 px-8 bg-rose-100 rounded-t-lg hover:bg-rose-50 "
+
+
+  return (
+    <main className="bg-rose-100 text-black my-auto flex-col flex flex-wrap ">
+      <Navigation id="Navbar" subpage={false} />
+      { scrollY > 500 &&
+      <div className={"transition duration-500 opacity-100"}>
+        <BackButton url="#Navbar" />
+      </div>}
+
+      <div className="w-screen bg-pink-50 flex-col pb-12">
+        <div className="w-10/12 h-full flex flex-col justify-center items-center mx-auto font-light">
+          {/* Logo */}
+          <div className="flex pt-12 justify-center w-full">
+            <h1 className=" mx-auto text-5xl pt-2 text-[#1a1a53]">🌸 | Portfolio</h1>
+            <div className=" my-auto">
+              <GitHubIcon url="https://github.com/tansimafiana/Portfolio" />
+            </div>
+          </div>
+
+          {/* Slide 1 */}
+          <div className="flex flex-col pt-20 w-full">
+            <h2 className="text-3xl text-center pb-6">Introduction</h2>
+            <p>Hello, my name is Tansima Kamal Fiana, and I am studying Software Development at Southern Alberta Institute of Technology. I am currently working on my portfolio, and I would like to showcase it here.</p>
+          </div>
+
+          {/* Slide 2 */}
+          <div className="flex flex-col pt-20 w-full space-y-6">
+            <h2 className="text-3xl text-center">Overview</h2>
+            <p>Welcome to Tansima Kamal Fiana&apos;s Creative Corner!
+
+Here, whimsy meets innovation, and imagination knows no bounds. As a Software Developer, I&apos;ve curated a collection of my finest works to share with you. From captivating illustrations to meticulously crafted designs, each piece embodies my dedication to creativity and excellence.
+
+Dive into my portfolio and explore a world where every pixel tells a story, where colors dance and concepts come to life. Whether you&apos;re seeking inspiration, collaboration, or simply a glimpse into my creative journey, you&apos;ll find it all here.
+
+So, grab a cup of creativity, take a stroll through my website, and let&apos;s embark on this artistic adventure together!</p>
+            
+          </div>
+
+          {/* Slide 3 */}
+          <div className="flex flex-col pt-20 w-full space-y-6">
+            <h2 className="text-3xl text-center">Tech Stack</h2>
+            <p>Crafted with React.js and Tailwind CSS, my portfolio website combines the power of modern web development with sleek design aesthetics. Utilizing React.js, I&apos;ve brought dynamic interactivity to every corner of the site, ensuring seamless navigation and a user-friendly experience. Meanwhile, Tailwind CSS lends its versatility and efficiency, allowing for beautifully crafted UI components and effortless responsiveness across devices.
+
+With this powerful tech stack, I&apos;ve created a platform that not only showcases my creative endeavors but also reflects my commitment to cutting-edge technology and design excellence. Explore with ease, indulge in the visual feast, and witness the magic of React.js and Tailwind CSS in action.</p>
+           
+          </div>
+
+         
+
+          {/* Slide 4 */}
+          <div className="flex flex-col pt-20 w-full space-y-6">
+            <h2 className="text-3xl text-center"> Features</h2>
+           
+
+ <p>🌸 Back to Top Button: Never lose your place in the creative landscape with the convenient &apos;Back to Top&apos; button. Effortlessly navigate through my portfolio and return to the beginning with just a click, ensuring seamless browsing from start to finish.</p>
+
+<p>🌸 Interactive Tabs: Dive deeper into my diverse range of projects with interactive tabs. Organized for ease of exploration, these tabs allow you to effortlessly switch between categories, unveiling a treasure trove of creativity with each selection.</p>
+
+<p>🌸 Dynamic Animations: Experience the magic of motion as dynamic animations breathe life into every corner of the website. From subtle fades to captivating transitions, these animations add an extra layer of engagement, ensuring your journey through my portfolio is nothing short of mesmerizing.</p>
+
+<p>🌸 Responsive Design: Explore my portfolio with confidence on any device, thanks to its responsive design. Whether you&apos;re browsing on a desktop, tablet, or smartphone, you&apos;ll enjoy a seamless and visually stunning experience tailored to your screen size.</p>
+
+<p>🌸 Interactive Elements: Engage with captivating interactive elements sprinkled throughout the site. From hover effects that reveal hidden details to clickable elements that invite exploration, these interactive features ensure that every visit to my portfolio is an immersive adventure.</p>
+
+<p>🌸 Seamless Navigation: Navigate effortlessly through my portfolio with intuitive controls and smooth transitions. Whether you&apos;re scrolling through projects or toggling between tabs, seamless navigation ensures that nothing stands between you and the captivating content waiting to be discovered.</p>
+
+
+            
+            
+            
+          </div>
+          {/* Slide 6 */}
+          <div className="flex flex-col pt-20 w-full space-y-6">
+            <h2 className="text-3xl text-center">Conclusion</h2>
+            <p>Take a stroll through my portfolio, where each piece reflects a chapter of my creative journey. I invite you to reflect on your own path as you explore. Connect with me to share your thoughts, offer feedback, or simply chat about creativity. Let&apos;s embark on this journey together. Thank you for visiting!</p>
+                        
+          </div>         
+        </div>
+      </div>
+    </main>
+  );
+}
+
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+     
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+}
+
+function useScroll() {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+    
+    // Add event listener
+    window.addEventListener("scroll", handleScroll);
+     
+    // Call handler right away so state gets updated with initial window size
+    handleScroll();
+    
+    // Remove event listener on cleanup
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+    }, []); // Empty array ensures that effect is only run on mount
+  return scrollY;
+}
--- tansimakamalfiana-portfolio/app/pages/PortfolioPage/page.js
+++ tansimakamalfiana-portfolio/app/pages/PortfolioPage/page.js
@@ -32,29 +32,55 @@ export default function ContactPage() {
         </div>
       </div>
       <div className="w-4/5 mx-auto">
-        <Slideshow slideData={[returnGameDevPage(), returnEngineeringUX()]}/>
+        <Slideshow slideData={[returnCapstone(), returnInternship(), returnEngineeringUX(), returnPortfolio(), returnGameDevPage()]}/>
       </div>
             
     </main>   
   );
 }
 
-function returnGameDevPage() {
+function returnCapstone() {
+  return (
+    /*<div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+      <button style={spanStyle}>{slideImage.caption}</button>
+    </div>*/
+    <div className={"flex align-middle h-[60vh]"}>
+      {/*<Image className="justify-center m-auto" alt="slideshow" src={slideImage.src} width={size.width ? size.width * 0.5 : 300} height={size.height ? size.height * 0.5 : 300} />*/}
+      <div className={"flex rounded-3xl justify-center w-10/12 h-4/5 m-auto"}>
+        <div className=" rounded-3xl -rotate-12 m-auto w-full h-4/5">
+          <Link href="../pages/CapstonePage">
+            <Image className=" rounded-3xl hover:scale-110 transition duration-300 shadow-xl" alt="Cyferd" src="/CatteryHomepage.png" fill style={{objectFit: "contain", borderRadius: "5px"}}  />
+          </Link>
+        </div>
+        <div className=" shadow-xl overflow-auto scroll-smooth p-4 rotate-12 my-auto bg-rose-50 opacity-90 rounded-3xl w-full h-4/5">
+          <h2 className=" font-bold text-center mx-auto mb-1 text-xl">Diamond Valley Cattery</h2>
+          <p className=" text-right text-sm">(React.js, Tailwind CSS, Firebase, Figma, Software Ideas Modeler (UML), Github)</p>
+          <button className="bg-white rounded-xl p-2 m-4 opacity-100 flex drop-shadow-md ml-auto mr-full"><Link href="../pages/CapstonePage">Case Study ➤</Link></button>
+          <p className="">Hello! I&apos;m Tansima Kamal Fiana, a Software Development student at SAIT. My team and I are currently working on developing the Diamond Valley Cattery website, dedicated to cat breeding, aiming to streamline operations for our client. We&apos;ve invested many hours into this project and continue to do so. Specifically, I&apos;ve been in charge of the front end, overseeing the UX/UI design process in Figma and implementing it using React.js and Tailwind CSS.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function returnInternship() {
   return (
       /*<div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
         <button style={spanStyle}>{slideImage.caption}</button>
       </div>*/
       <div className={"flex align-middle h-[60vh]"}>
         {/*<Image className="justify-center m-auto" alt="slideshow" src={slideImage.src} width={size.width ? size.width * 0.5 : 300} height={size.height ? size.height * 0.5 : 300} />*/}
-        <div className={"flex justify-center w-10/12 h-4/5 m-auto"}>
-          <div className="rounded-xl -rotate-12 m-auto w-full h-4/5">
-            <Link href="../pages/GameDevPage">
-              <Image className=" hover:scale-110 transition duration-300" alt="GameDev" src="/Zombie3D_New.gif" fill style={{objectFit: "contain", borderRadius: "5px"}}  />
+        <div className={"flex rounded-3xl justify-center w-10/12 h-4/5 m-auto"}>
+          <div className=" shadow-xl rounded-3xl -rotate-12 m-auto w-full h-4/5">
+            <Link href="../pages/InternshipPage">
+              <Image className="bg-white rounded-3xl hover:scale-110 transition duration-300" alt="Cyferd" src="/Cyferd.png" fill style={{objectFit: "contain", borderRadius: "5px"}}  />
             </Link>
           </div>
-          <div className=" p-4 rotate-12 my-auto bg-white opacity-70 rounded-xl w-full h-4/5">
-            <h2 className=" font-bold text-center mx-auto mb-1 text-xl">Game Development</h2>
-            <p>Game development things...</p>
+          <div className=" shadow-xl overflow-auto scroll-smooth p-4 rotate-12 my-auto bg-rose-50 opacity-90 rounded-3xl w-full h-4/5">
+            <h2 className=" font-bold text-center mx-auto mb-1 text-xl">Cyferd Internship</h2>
+            <p className=" text-center text-sm"> Pause and Reflect App</p>
+            <button className="bg-white rounded-xl p-2 m-4 opacity-100 flex drop-shadow-md ml-auto mr-full"><Link href="../pages/InternshipPage">Case Study ➤</Link></button>
+            <p className="">During my internship, I developed Pause and Reflect, an app on the Cyferd platform. It&apos;s aimed at reducing stress and promoting well-being by encouraging users to take a moment to pause, reflect, and reconnect with themselves. Designed with busy professionals and college students in mind, the app provides personalized self-reflection activities. While I&apos;m unable to provide a live demo, imagine features like sleep pattern tracking charts, personalized wellness profiles, tools for maintaining physical health, and visual aids for better understanding. This project significantly improved my coding and problem-solving abilities, and I&apos;m especially grateful to Sara Al-Seaidy for her guidance. My certificate is inside. I also received a certificate for my work on this project.</p>
           </div>
         </div>
       </div>
@@ -63,22 +89,72 @@ function returnGameDevPage() {
 
 function returnEngineeringUX() {
   return (
-      /*<div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
-        <button style={spanStyle}>{slideImage.caption}</button>
-      </div>*/
-      <div className={"flex align-middle h-[60vh]"}>
-        {/*<Image className="justify-center m-auto" alt="slideshow" src={slideImage.src} width={size.width ? size.width * 0.5 : 300} height={size.height ? size.height * 0.5 : 300} />*/}
-        <div className={"flex justify-center w-10/12 h-4/5 m-auto"}>
-          <div className="rounded-xl -rotate-12 m-auto w-full h-4/5">
-            <Link href="../pages/EngineeringPage">
-              <Image className=" hover:scale-110 transition duration-300" alt="EngineeringUX" src="/HMI 11_ - Home Page.png" fill style={{objectFit: "contain", borderRadius: "5px"}}  />
-            </Link>
-          </div>
-          <div className=" p-4 rotate-12 my-auto bg-white opacity-70 rounded-xl w-full h-4/5">
-            <h2 className=" font-bold text-center mx-auto mb-1 text-xl">Engineering UX</h2>
-            <p>Engineering UX things...</p>
-          </div>
+    /*<div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+      <button style={spanStyle}>{slideImage.caption}</button>
+    </div>*/
+    <div className={"flex align-middle h-[60vh]"}>
+      {/*<Image className="justify-center m-auto" alt="slideshow" src={slideImage.src} width={size.width ? size.width * 0.5 : 300} height={size.height ? size.height * 0.5 : 300} />*/}
+      <div className={"flex rounded-3xl justify-center w-10/12 h-4/5 m-auto"}>
+        <div className=" shadow-xl rounded-3xl -rotate-12 m-auto w-full h-4/5">
+          <Link href="../pages/InternshipPage">
+            <Image className="rounded-3xl hover:scale-110 transition duration-300" alt="Cyferd" src="/HMI 11_ - Home Page.png" fill style={{objectFit: "contain", borderRadius: "5px"}}  />
+          </Link>
+        </div>
+        <div className=" shadow-xl overflow-auto scroll-smooth p-4 rotate-12 my-auto bg-rose-50 opacity-90 rounded-3xl w-full h-4/5">
+          <h2 className=" font-bold text-center mx-auto mb-1 text-xl">Engineering UX</h2>
+          <p className=" text-center text-sm">(Figma, Sketches)</p>
+          <button className="bg-white rounded-xl p-2 m-4 opacity-100 flex drop-shadow-md ml-auto mr-full"><Link href="../pages/EngineeringPage">Case Study ➤</Link></button>
+          <p className="">Partnering with a friend from Mechanical Engineering, we&apos;re integrating an Automated Storage Retrieval System (ASRS) with an ABB IRB140 Robot Arm. My role involves designing a user-friendly Human-Machine Interface (HMI) using Figma. Through this collaboration, I&apos;m exploring new concepts and pushing the boundaries of my skills. Stay tuned for updates on our progress!</p>
         </div>
       </div>
+    </div>
+  );
+};
+
+function returnPortfolio() {
+  return (
+    /*<div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+      <button style={spanStyle}>{slideImage.caption}</button>
+    </div>*/
+    <div className={"flex align-middle h-[60vh]"}>
+      {/*<Image className="justify-center m-auto" alt="slideshow" src={slideImage.src} width={size.width ? size.width * 0.5 : 300} height={size.height ? size.height * 0.5 : 300} />*/}
+      <div className={"flex rounded-3xl justify-center w-10/12 h-4/5 m-auto"}>
+        <div className=" shadow-xl rounded-3xl -rotate-12 m-auto w-full h-4/5">
+          <Link href="../pages/PersonalPortfolioPage">
+            <Image className="rounded-3xl hover:scale-110 transition duration-300" alt="Cyferd" src="/PortfolioSnap.jpeg" fill style={{objectFit: "contain", borderRadius: "5px"}}  />
+          </Link>
+        </div>
+        <div className=" shadow-xl overflow-auto scroll-smooth p-4 rotate-12 my-auto bg-rose-50 opacity-90 rounded-3xl w-full h-4/5">
+          <h2 className=" font-bold text-center mx-auto mb-1 text-xl">Introducing Tansima Kamal Fiana&apos;s Portfolio Showcase</h2>
+          <p className=" text-center text-sm">(Figma, Sketches)</p>
+          <button className="bg-white rounded-xl p-2 m-4 opacity-100 flex drop-shadow-md ml-auto mr-full"><Link href="../pages/PersonalPortfolioPage">Case Study ➤</Link></button>
+          <p className="">Hello! I&apos;m Tansima Kamal Fiana, a Software Development student at SAIT, and this is my portfolio. Dive into a world where creativity meets technology. Crafted with React.js and Tailwind CSS, my portfolio offers seamless navigation and captivating animations. Explore my work and let&apos;s embark on this creative journey together. Thank you for visiting!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function returnGameDevPage() {
+  return (
+    /*<div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+      <button style={spanStyle}>{slideImage.caption}</button>
+    </div>*/
+    <div className={"flex align-middle h-[60vh]"}>
+      {/*<Image className="justify-center m-auto" alt="slideshow" src={slideImage.src} width={size.width ? size.width * 0.5 : 300} height={size.height ? size.height * 0.5 : 300} />*/}
+      <div className={"flex rounded-3xl justify-center w-10/12 h-4/5 m-auto"}>
+        <div className=" shadow-xl rounded-3xl -rotate-12 m-auto w-full h-4/5">
+          <Link href="../pages/GameDevPage">
+            <Image className="rounded-3xl hover:scale-110 transition duration-300" alt="Cyferd" src="/Zombie3D_New.gif" fill style={{objectFit: "contain", borderRadius: "5px"}}  />
+          </Link>
+        </div>
+        <div className=" shadow-xl overflow-auto scroll-smooth p-4 rotate-12 my-auto bg-rose-50 opacity-90 rounded-3xl w-full h-4/5">
+          <h2 className=" font-bold text-center mx-auto mb-1 text-xl">Game Development UX</h2>
+          <p className=" text-sm text-center">(Adobe Photoshop, Blender, Mixamo, Lo-fi Sketches)</p>
+          <button className="bg-white rounded-xl p-2 m-4 opacity-100 flex drop-shadow-md ml-auto mr-full"><Link href="../pages/GameDevPage">Case Study ➤</Link></button>
+          <p className="">Using 3D modelling tools to bring my textures to life, I designed the layouts for models and maps for an indie game called Zomboid.</p>
+        </div>
+      </div>
+    </div>
   );
 };
