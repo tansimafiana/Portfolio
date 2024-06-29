7fb6054e94453265df21218e6783378e72f915d1
tansimafiana <tansima.fiana@edu.sait.ca>
2024-06-29 00:55:47 -0600

    Added Mobile Hamburger Bar

--- tansimakamalfiana-portfolio/app/navigation/page.js
+++ tansimakamalfiana-portfolio/app/navigation/page.js
@@ -4,12 +4,20 @@ import Image from "next/image";
 import Link from "next/link";
 import Buttons from "../components/Buttons";
 import { IoMdHome } from "react-icons/io";
-import React, { useState, useEffect } from "react"
+import { RxHamburgerMenu } from "react-icons/rx";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import React, { useState, useEffect, useRef } from "react"
 
 export default function Navigation({subpage}) {
   
   const size = useWindowSize();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const sidebarRef = useRef(null);
   let url;
+
+  function handleCloseSidebar() {
+    setIsMenuOpen(false);
+  }
   
   if (typeof window !== "undefined") {
     url = String(window.location);
@@ -27,18 +35,62 @@ export default function Navigation({subpage}) {
     console.log(ID);
   }, [url]);
 
+  useEffect(() => {
+    if (size.width >= 800) {
+      setIsMenuOpen(false);
+    }
+  }, [size]);
+
+  useEffect(() => {
+    if (!isMenuOpen) return;
+
+    function handleClick(event) {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        setIsMenuOpen(false);
+      }
+    }
+
+    window.addEventListener("mouseup", handleClick);
+    return () => { window.removeEventListener("mouseup", handleClick); }
+  }, [isMenuOpen])
+
   return (
-    <main className="bg-white h-24 w-screen flex">
-      <Link href="../" className="my-auto px-6 pb-6">
-        <IoMdHome className="w-8 h-8 mt-6 ml-6 hover:scale-110 transition duration-300"/>
-      </Link>
-      <div className="ml-auto">
-        <div className=" flex top-0 right-0 h-16 w-full justify-end p-4">
-          <Buttons className="text-2xl p-2 hover:text-[#ffee32]" text={size.width} href={ subpage ? "../PortfolioPage" : "../pages/PortfolioPage"} scale={ ID === 1 ? " text-[#ffee32] scale-110 " : " hover:scale-110 "}/>
-          <Buttons  className="text-2xl p-2 hover:text-[#f77f00]" text="RESUME" href={ subpage ? "../ResumePage" : "../pages/ResumePage"} scale={ ID === 2 ? " text-[#f77f00] scale-110 " : " hover:scale-110 "}/>
-          <Buttons  className="text-2xl p-2 hover:text-[#ED4672]" text="CONTACT" href={ subpage ? "../ContactPage" : "../pages/ContactPage"} scale={ ID === 3 ? " text-[#ED4672] scale-110 " : " hover:scale-110 "}/>
+    <main>
+      { size.width < 800 ?
+        <div className="relative z-50">
+          <button onClick={() => setIsMenuOpen(true)} className="z-10">
+            <RxHamburgerMenu className="size-8 mt-6 ml-6 hover:scale-110 transition duration-300" />
+          </button>
+          <div ref={sidebarRef} className={"bg-white shadow-lg w-full h-screen fixed top-0 left-0 max-w-96 border-r transition duration-500 " + (isMenuOpen ? "  " : " -translate-x-[100%] ")}>
+            <button onClick={() => setIsMenuOpen(false)} className="z-10">
+              <div className="flex">
+                <Link href="../" className="my-auto pb-6">
+                  <IoMdHome className="w-8 h-8 mt-6 ml-6 hover:scale-110 transition duration-300"/>
+                </Link>
+                <MdKeyboardArrowLeft className="size-10 mt-6 right-0 absolute mr-6 hover:scale-110 transition duration-300" />
+              </div>
+            </button>
+            <div className=" ml-6">
+              <Buttons className="text-2xl p-2 hover:text-[#ffee32]" text="PORTFOLIO" href={ subpage ? "../PortfolioPage" : "../pages/PortfolioPage"} scale={ ID === 1 ? " text-[#ffee32] scale-110 " : " hover:scale-110 "}/>
+              <Buttons  className="text-2xl p-2 hover:text-[#f77f00]" text="RESUME" href={ subpage ? "../ResumePage" : "../pages/ResumePage"} scale={ ID === 2 ? " text-[#f77f00] scale-110 " : " hover:scale-110 "}/>
+              <Buttons  className="text-2xl p-2 hover:text-[#ED4672]" text="CONTACT" href={ subpage ? "../ContactPage" : "../pages/ContactPage"} scale={ ID === 3 ? " text-[#ED4672] scale-110 " : " hover:scale-110 "}/>
+            </div>
+          </div>
         </div>
-      </div>
+        :
+        <div className="bg-white h-24 w-screen flex">
+          <Link href="../" className="my-auto px-6 pb-6">
+            <IoMdHome className="w-8 h-8 mt-6 ml-6 hover:scale-110 transition duration-300"/>
+          </Link>
+          <div className="ml-auto">
+            <div className=" flex top-0 right-0 h-16 w-full justify-end p-4">
+              <Buttons className="text-2xl p-2 hover:text-[#ffee32]" text="PORTFOLIO" href={ subpage ? "../PortfolioPage" : "../pages/PortfolioPage"} scale={ ID === 1 ? " text-[#ffee32] scale-110 " : " hover:scale-110 "}/>
+              <Buttons  className="text-2xl p-2 hover:text-[#f77f00]" text="RESUME" href={ subpage ? "../ResumePage" : "../pages/ResumePage"} scale={ ID === 2 ? " text-[#f77f00] scale-110 " : " hover:scale-110 "}/>
+              <Buttons  className="text-2xl p-2 hover:text-[#ED4672]" text="CONTACT" href={ subpage ? "../ContactPage" : "../pages/ContactPage"} scale={ ID === 3 ? " text-[#ED4672] scale-110 " : " hover:scale-110 "}/>
+            </div>
+          </div>
+        </div>
+      }
     </main>
   );
 }
